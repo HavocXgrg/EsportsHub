add2dab3fe0fc3cb52b7b2cdabcdbea99b11b929
bishwa <108havocx108@gmail.com>
2025-09-24 19:17:12 +0545

    added a pages

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ILoginProps {}
+
+const Login: React.FunctionComponent<ILoginProps> = () => {
+  return <div>login page</div>;
+};
+
+export default Login;
---
+++ src/pages/Schedule/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IScheduleProps {}
+
+const Schedule: React.FunctionComponent<IScheduleProps> = (props) => {
+  return <div>Schedule</div>;
+};
+
+export default Schedule;
